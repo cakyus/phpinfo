4256bf3372f5c066a309abdc04808634d2a5c54c
cakyus <cakyus@gmail.com>
2011-06-15 21:10:10 +0700

    add search input in functions.php

--- functions.php
+++ functions.php
@@ -8,12 +8,33 @@ $rows = $rows['internal'];
 sort($rows);
 $site = 'http://127.0.1.1/php/manual/php/';
 
+include('header.php');
+
+// search box
+
+if (empty($_GET['q'])) {
+    $filterName = '';
+} else {
+    $filterName = $_GET['q'];
+}
+
+echo '<form action="" method="GET">'
+    .'<input type="text"'
+    .' name="q" value="'.htmlentities($filterName).'"'
+    .' />'
+    .'</form>';
+    
 foreach ($rows as $name => $item) {
     $functionName = $item;
     $functionName = str_replace('_','-',$functionName);
-    echo '<div id="function.'.$functionName.'">'
-        .'<a href="'.$site.'function.'.$functionName.'.html">'
-            .$item.'</a>'
-        .'</div>';
+    if (    empty($filterName)
+        ||  strpos($functionName, $filterName) !== false
+        ) { 
+        echo '<div class="function" id="function.'.$functionName.'">'
+            .'<a href="'.$site.'function.'.$functionName.'.html">'
+                .$item.'</a>'
+            .'</div>';
+    }
 }
 
+include('footer.php');
---
+++ header.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>phpinfo</title>
+        <style>
+            #theme {
+                width: 95%;
+                text-align: left;
+            }
+            a {
+                text-decoration: none;
+            }
+            .hidden {
+                display: none;
+            }
+        </style>
+    </head>
+    <body><center><div id="theme">
+    <div id="theme-header">
+    
+    </div><div id="theme-content">
+
