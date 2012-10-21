84b79265d7350925d34421d5675a3f200684d354
cakyus <cakyus@gmail.com>
2012-10-21 17:06:42 +0700

    add a configuration file

--- classes.php
+++ classes.php
@@ -1,4 +1,4 @@
-<?php
+<?php include('config.php');
 
 // @todo configuration for manual site 
 // @todo configuration for manual type, eg. single html file, many html file
@@ -6,15 +6,18 @@
 $rows = get_declared_classes();
 
 sort($rows);
-$site = 'http://127.0.1.1/php/manual/php/';
+$site = PHPINFO_MANUAL_URL;
 
 foreach ($rows as $name => $item) {
     $functionName = $item;
     $functionName = str_replace('_','-',$functionName);
     $functionName = strtolower($functionName);
     echo '<div id="class.'.$functionName.'">'
-        .'<a href="'.$site.'class.'.$functionName.'.html">'
-            .$item.'</a>'
+        .'<a href="'
+			.PHPINFO_MANUAL_URL
+			.'class.'.$functionName
+			.'.'.PHPINFO_MANUAL_TYPE
+			.'">'.$item.'</a>'
         .'</div>';
 }
 
---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+
+define('PHPINFO_MANUAL_URL', 'manual/php-chunked-xhtml/');
+define('PHPINFO_MANUAL_TYPE', 'html');
